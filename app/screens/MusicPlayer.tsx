7839a5ed0496ce38291bc2339afb0efb61c4c7d1
0biglife <kimchoon42@gmail.com>
2021-11-30 15:29:50 +0900

    MusicPlayer-TrackImage with Text

--- app/screens/MusicPlayer.tsx
+++ app/screens/MusicPlayer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Dimensions, TouchableOpacity} from 'react-native';
+import {Dimensions, TouchableOpacity, View} from 'react-native';
+import Slider from '@react-native-community/slider';
 import styled from 'styled-components/native';
 
 import IonIcon from 'react-native-vector-icons/Ionicons';
@@ -20,13 +21,39 @@ const { width, height } = Dimensions.get('window');
 
 const SafeAreaViewContainer = styled.SafeAreaView`
   flex: 1;
-  background-color: #222831;
+  background-color: #ffffff;
 `;
 
 const MainContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+const ArtworkWrapper = styled.View`
+  width: 300px;
+  height: 300px;
+  margin-bottom: 25px;
+`;
+
+const ArtworkImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+  color: black;
+`;
+
+const ArtistText = styled.Text`
+  font-size: 16px;
+  font-weight: 200;
+  text-align: center;
+  color: black;
 `;
 
 const BottomContainer = styled.View`
@@ -66,7 +93,13 @@ const MusicPlayer = () => {
   return (
     <SafeAreaViewContainer>
       <MainContainer>
-
+        <ArtworkWrapper>
+          <ArtworkImage source={require('../assets/artwork/cover2.jpeg')} />
+        </ArtworkWrapper>
+        <View>
+          <TitleText>Song Title</TitleText>
+          <ArtistText>Song Artist Name</ArtistText>
+        </View>
       </MainContainer>
       <BottomContainer>
         <BottomControls>
@@ -75,6 +108,12 @@ const MusicPlayer = () => {
           </TouchableOpacity>
           <TouchableOpacity onPress={() => { }}>
             <IonIcon name="repeat" size={30} color="#777777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <IonIcon name="share-outline" size={30} color="#777777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <IonIcon name="ellipsis-horizontal" size={30} color="#777777" />
           </TouchableOpacity>
         </BottomControls>
       </BottomContainer>
