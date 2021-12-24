926588b614a04f8a04573135991c2c2f5cb467ba
0biglife <kimchoon42@gmail.com>
2021-12-24 17:09:01 +0900

    GOOGLESIGNIN/installation and button setting

--- app/screens/Auth/login.tsx
+++ app/screens/Auth/login.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import {View, Text} from 'react-native';
 import styled from 'styled-components/native';
 import {Button} from '../../components';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const Container = styled.View`
   flex: 1;
@@ -18,11 +23,84 @@ const Title = styled.Text`
 `;
 
 const loginView = ({navigation}) => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId:
+        '21966285335-pd59r4mk54v02nd8v5k2kem3gt1th3fl.apps.googleusercontent.com',
+      //reverse : com.googleusercontent.apps.21966285335-pd59r4mk54v02nd8v5k2kem3gt1th3fl
+      // iosClientId:
+      //   '21966285335-0r4fqc0aoe84encol860j1q7l95mnt1o.apps.googleusercontent.com',
+      offlineAccess: false,
+      forceCodeForRefreshToken: true,
+    });
+    isSignedIn();
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('due_______', userInfo);
+      setUser(userInfo);
+    } catch (error) {
+      console.log('MESSAGE', error.message);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('User Cancelled the Login Flow');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('Signing In');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('Play services Not Availbale');
+      } else {
+        console.log('Some other Error happened');
+      }
+    }
+  };
+  const isSignedIn = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    if (!!isSignedIn) {
+      getCurrentUserInfo();
+    } else {
+      console.log('Please Login');
+    }
+  };
+
+  const getCurrentUserInfo = async () => {
+    try {
+      const userinfo = await GoogleSignin.signInSilently();
+      setUser(userinfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+        Alert.alert('User has not signed in yet!');
+        console.log('User has not signed in yet!');
+      } else {
+        Alert.alert('Something went wrong');
+        console.log('Something went wrong');
+      }
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setUser({});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container>
       <Title>Login View</Title>
       <Button title="Sign In" onPress={() => navigation.navigate('Signup')} />
       <Button title="Log In" onPress={() => navigation.navigate('MainTab')} />
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
     </Container>
   );
 };
