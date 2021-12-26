3edc804896c2bc49ed16afdae6a1f6ac478baa3f
0biglife <kimchoon42@gmail.com>
2021-12-26 21:26:42 +0900

    SOCIAL LOGIN/GOOGLE/ANDROID Done

--- android/app/src/main/java/com/connectym_ts/MainApplication.java
+++ android/app/src/main/java/com/connectym_ts/MainApplication.java
@@ -4,15 +4,17 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativepagerview.PagerViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
+// import com.reactnativepagerview.PagerViewPackage;
+// import org.reactnative.camera.RNCameraPackage;
+// import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import androidx.multidex.MultiDexApplication;
 
 public class MainApplication extends Application implements ReactApplication {
 
