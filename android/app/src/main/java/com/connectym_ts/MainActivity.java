5667eab2fccaf7bfe4e682eef82fd4093b7368e2
0biglife <kimchoon42@gmail.com>
2022-01-07 11:51:04 +0900

    SplashScreen/iOS AOS Setting Done && react-native-make Installation Done

--- android/app/src/main/java/com/connectym_ts/MainActivity.java
+++ android/app/src/main/java/com/connectym_ts/MainActivity.java
@@ -2,12 +2,20 @@ package com.connectym_ts;
 
 import com.facebook.react.ReactActivity;
 
+import org.devio.rn.splashscreen.SplashScreen; 
+
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this, R.style.SplashScreenTheme);
+      super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "ConnectyM_TS";
--- ios/ConnectyM_TS/AppDelegate.m
+++ ios/ConnectyM_TS/AppDelegate.m
@@ -12,6 +12,9 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+#import "RNSplashScreen.h" // react-native-splash-screen
+
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -47,6 +50,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show]; //react-natvie-splash-screen
   return YES;
 }
 
