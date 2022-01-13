e482cd4113808258fabec4e2cabde17b8c748013
0biglife <kimchoon42@gmail.com>
2022-01-13 20:52:40 +0900

    SearchView/Cell UI Changed

--- app/navigations/Types.tsx
+++ app/navigations/Types.tsx
@@ -1,4 +1,4 @@
-import { ImageSourcePropType } from "react-native";
+import {ImageSourcePropType} from 'react-native';
 
 export type AuthStackParamList = {
   Login: undefined;
--- app/screens/search/search.tsx
+++ app/screens/search/search.tsx
@@ -1,8 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native-gesture-handler';
+import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import {AxiosResponse} from 'axios';
 import apiClient from '../../apis/service/client';
+import {Photo} from '../../apis/model/data';
+import {SearchParamsList} from '../../navigations/Types';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RouteProp, useNavigation} from '@react-navigation/native';
+import {profileView} from '../../screens';
 
 const SafeContainer = styled.SafeAreaView`
   flex: 1;
@@ -11,18 +16,18 @@ const SafeContainer = styled.SafeAreaView`
 const MainContainer = styled.View`
   flex: 1;
   background-color: ${prop => prop.theme.color.bg};
-  /* align-items: center;
-  justify-content: center; */
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
 `;
 
 const SearchTextInput = styled.TextInput`
   align-self: center;
-  width: 94%;
+  width: 100%;
   height: 40px;
   border-width: 1px;
   border-radius: 8px;
   padding-left: 20px;
-  margin: 10px;
   border-color: lightgray;
   background-color: ${prop => prop.theme.color.bg};
 `;
@@ -35,18 +40,44 @@ const Title = styled.Text`
 `;
 
 const ListView = styled.View`
-  height: 20;
+  height: 8px;
   width: 100%;
+  /* background-color: ${prop => prop.theme.color.bg}; */
+  background-color: white;
+`;
+
+const CellContainer = styled.View`
+  /* flex-direction: row; */
+  /* align-items: center; */
+  width: 100%;
+  height: 120px;
+  border-radius: 8px;
+  margin-top: 8px;
+  padding: 8px;
   background-color: lightgray;
 `;
 
-const searchView = () => {
+const ProfileImage = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-width: 1px;
+  border-color: gray;
+  border-radius: 25px;
+`;
+
+export interface SearchProps {
+  navigation: StackNavigationProp<SearchParamsList, 'Search'>;
+  route: RouteProp<SearchParamsList, 'Search'>;
+}
+
+const searchView: React.FC<SearchProps> = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [masterData, setMasterdData] = useState([]);
   const [search, setSearch] = useState('');
+  const navigation = useNavigation();
 
   useEffect(() => {
-    apiClient.get('/posts').then((response: AxiosResponse) => {
+    apiClient.get<Photo[]>('/photos').then((response: AxiosResponse) => {
       try {
         setFilteredData(response.data);
         setMasterdData(response.data);
@@ -58,12 +89,13 @@ const searchView = () => {
 
   const ItemView = ({item}) => {
     return (
-      <Title>
-        {item.id}
-        {': '}
-        {item.title}
-      </Title>
-    );
+      <CellContainer>
+        <TouchableOpacity onPress={() => navigation.navigate(profileView)}>
+          <ProfileImage source={{uri: item.url}} />
+        </TouchableOpacity>
+        <Title>{item.title}</Title>
+      </CellContainer>
+    )
   };
 
   const ItemSeperatorView = () => {
@@ -96,8 +128,8 @@ const searchView = () => {
         />
         <FlatList
           data={filteredData}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeperatorView}
+          keyExtractor={(item, index) => index}
+          // ItemSeparatorComponent={ItemSeperatorView}
           renderItem={ItemView}
         />
       </MainContainer>
